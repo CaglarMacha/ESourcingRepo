e4f0e3eaa508f595ef988795c096b469ae48d703
CaglarMacha <caglar.ayhan@boun.edu.tr>
2021-12-16 23:00:25 +0300

    Mongo Client was added

---
+++ ESourcing.Products/Data/Interfaces/IProductContext.cs
@@ -0,0 +1,15 @@
+﻿using ESourcing.Products.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESourcing.Products.Data.Interfaces
+{
+    public interface IProductContext
+    {
+        //Mongo Db Context işlemini yapıyoruz Mongo Client e bağlanıyoruz
+        IMongoCollection<Product> Products
+    }
+}
