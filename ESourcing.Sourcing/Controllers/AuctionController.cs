de12590416fbe4e5349ff07d83bcfc46e1c9bcdb
CaglarMacha <caglar.ayhan@boun.edu.tr>
2021-12-18 03:37:04 +0300

    AuctionController was edited

--- ESourcing.Sourcing/Controllers/AuctionController.cs
+++ ESourcing.Sourcing/Controllers/AuctionController.cs
@@ -1,6 +1,8 @@
-﻿using ESourcing.Sourcing.Entities;
+﻿using DnsClient.Internal;
+using ESourcing.Sourcing.Entities;
 using ESourcing.Sourcing.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,10 +16,12 @@ namespace ESourcing.Sourcing.Controllers
     public class AuctionController : ControllerBase
     {
         private readonly IAuctionRepository _auctionRepository;
+        private readonly ILogger<AuctionController> _logger;
 
-        public AuctionController(IAuctionRepository auctionRepository)
+        public AuctionController(IAuctionRepository auctionRepository, ILogger<AuctionController> logger)
         {
             _auctionRepository = auctionRepository;
+            _logger = logger;
         }
 
         [HttpGet]
@@ -27,6 +31,39 @@ namespace ESourcing.Sourcing.Controllers
             var auction = await _auctionRepository.GetAuctions ();
             return Ok(auction);
         }
+        [HttpGet("id:lenght(24)",Name ="GetAuction")]
+        [ProducesResponseType(typeof(IEnumerable<Auction>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult<Auction>> GetAuction(string id)
+        {
+            var auction = await _auctionRepository.GetAuction(id);
+            if (auction == null)
+            {
+                _logger.LogError($"Auction with id: {id} has not been found in db");
+                return NotFound();
+            }
+            return Ok(auction);
+        }
+        [HttpPost]
+        [ProducesResponseType(typeof(IEnumerable<Auction>),(int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Auction>> CreateAuction ([FromBody]Auction auction)
+        {
+            await _auctionRepository.Create(auction);
+            return CreatedAtRoute("GetAction", new {id=auction.Id },auction);
+        }
+        [HttpPut]
+        [ProducesResponseType(typeof(IEnumerable<Auction>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Auction>> UpdateAuction([FromBody] Auction auction)
+        {
+            return Ok(await _auctionRepository.Update(auction));  
+        }
+        [HttpDelete]
+        [ProducesResponseType(typeof(IEnumerable<Auction>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Auction>> DeleteAuction(string id)
+        {
+            return Ok(await _auctionRepository.Delete(id));
+        }
+
     }
 
 }
